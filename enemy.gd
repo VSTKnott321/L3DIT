3774a262dc882ff550800df24e6826b9092aae63
VSTKnott321 <vk05@mahurangi.school.nz>
2026-03-02 14:27:05 +1300

    added new weapon
    i added a new weapon model

---
+++ enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+class_name Enemy
+
+
+func _physics_process(delta):
+	pass
+
+
+
+
+
+func _on_sight_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_player_body_exited(body):
+	pass # Replace with function body.
+
+
+func _on_sight_body_exited(body):
+	pass # Replace with function body.
