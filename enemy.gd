9fff0ddd34cc81f69b4a244f07f5f24e41788431
VSTKnott321 <vk05@mahurangi.school.nz>
2026-06-08 10:15:44 +1200

    working on the enemy ai
    ive been working  on my enemy ai, and the tracking

--- enemy.gd
+++ enemy.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 class_name Enemy
 
 
+var player: Player = null
+
 func _physics_process(delta):
 	pass
 
@@ -9,8 +11,11 @@ func _physics_process(delta):
 
 
 
-func _on_sight_body_entered(body):
-	pass # Replace with function body.
+func _on_sight_body_entered(body: Node2D) -> void:
+	if body is Player:
+		if player == null:
+			player = body
+			print(name + "found the player") 
 
 
 func _on_player_body_exited(body):
@@ -18,4 +23,7 @@ func _on_player_body_exited(body):
 
 
 func _on_sight_body_exited(body):
-	pass # Replace with function body.
+	if body is Player:
+		if player != null:
+			player = body
+			print(name + "found the player")
