4d77ac06ca02dc55d4d50a05301f7f1b6603028a
VSTKnott321 <vk05@mahurangi.school.nz>
2026-06-17 12:55:40 +1200

    created basic menus system
    created the basis for a menus system, with a button to start the game, and a button to quit the game

--- main_menus.gd
+++ main_menus.gd
@@ -9,3 +9,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://levels/level1.tscn")
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
