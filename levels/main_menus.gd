58c00afa2865f1bbdf6676b662b51a50de9b65c5
VSTKnott321 <vk05@mahurangi.school.nz>
2026-06-22 09:32:10 +1200

    level progression system with ehalth and damage
    made the comptete health and damage system, aswell as the lvel progression system, and a tile map system

--- levels/main_menus.gd
+++ levels/main_menus.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://levels/level1.tscn")
+	get_tree().change_scene_to_file("res://levels/tutorial.tscn")
 
 
 func _on_options_pressed():
