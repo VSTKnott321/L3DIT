16e81d6eb4535f8be63b4c477467ac0eba65a716
VSTKnott321 <vk05@mahurangi.school.nz>
2026-06-25 11:44:10 +1200

    added enemy damage from the projectiles
    enemys now take damge from player projectiles and die at Zero health

--- enemy.gd
+++ enemy.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 class_name Enemy
 
+var health = 200
+var speed = 0.5
 
-var speed = 0.25
 var player_chase = false
 var player = null
 
@@ -10,7 +11,15 @@ var player = null
 func _physics_process(delta):
 	if player_chase:
 		position = lerp(position, player.position, speed * delta)
-		#position += (player.position - position)/speed
+		look_at(player.global_position)
+		if(player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_v = true
+		else:
+			$AnimatedSprite2D.flip_v = false
+		
+#replace queue free with death function
+	if health <= 0:
+		queue_free()
 
 
 
@@ -24,3 +33,8 @@ func _on_sight_body_entered(body):
 func _on_sight_body_exited(body):
 	player = null
 	player_chase = false
+
+
+func _on_dmg_hitbox_body_entered(body):
+	health = health - 25
+	# replace - 25 with acutal damage function
